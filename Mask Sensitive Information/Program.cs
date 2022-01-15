3f0578c39c750efadeff10fb460165a8d53da05c
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-01-15 13:35:05 -0600

    Add project files.

---
+++ Mask Sensitive Information/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mask_Sensitive_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me a secret...");
+
+            var secret = Console.ReadLine();
+
+            if (secret.Length > 4)
+            {
+                Console.WriteLine(
+                    string.Concat(
+                        "".PadLeft(12, '*'),
+                        secret.Substring(secret.Length - 4)
+                        )
+                    );
+            }
+            else
+            {
+                Console.WriteLine("Please tell me a longer secret!");
+            }
+        }
+    }
+}
